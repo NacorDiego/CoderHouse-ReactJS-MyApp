00fc365b22dbb8850ce3935741e0f402584a8296
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2022-06-24 19:14:03 -0300

    Desafio 4 - Commit 3: Se corrige ruta en importación del componente 'Navbar' en 'App'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/Navbar/Navbar';
 import StoreCatalog from './components/StoreCatalog/StoreCatalog';
 
 function App() {
